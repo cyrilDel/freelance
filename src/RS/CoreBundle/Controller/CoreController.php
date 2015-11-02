845c5a024c3f16a5c915c7616e65f40cf70e62b9
cyrilDel <delage.cyril@icloud.com>
2015-11-02 04:36:20 +0100

    swiftmailer_formulaire

--- src/RS/CoreBundle/Controller/CoreController.php
+++ src/RS/CoreBundle/Controller/CoreController.php
@@ -16,23 +16,44 @@ class CoreController extends Controller
     // La page d'accueil   
     public function indexAction(Request $request)
     {
-        // On créer l'objet Mailer
+        // On crÃ©er l'objet Mailer
         $mailer = new Mailer();
 
-        // On créer le FormBuilder avec le service form factory
+        // On crÃ©er le FormBuilder avec le service form factory
         $form = $this->get('form.factory')->create(new MailerType(), $mailer);
 
         if ($form->handleRequest($request)->isValid()) {
             $em = $this->getDoctrine()->getManager();
             $em->persist($mailer);
             $em->flush();
+            
+            // Ici le mail de validation
+            $message = \Swift_Message::newInstance()
+                ->setSubject('Nouveau message sur creer-monsite.fr')
+                ->setFrom('cdelage.dev@gmail.com')
+                ->setTo('cdelage.dev@gmail.com')
+                ->setCharset('utf-8')
+                ->setContentType('text/html')
+                ->setBody( $this->renderView(
+                    'RSCoreBundle:Core:mail.html.twig',
+                    array(
+                        'ip' => $request->getClientIp(),
+                        'courriel' => $form->get('courriel')->getData(),
+                        'message' => $form->get('message')->getData()
+                    )
+                )
+            );
 
-            $request->getSession()->getFlashBag()->add('notice', 'Message envoyé avec succés!');
+            $this->get('mailer')->send($message);
+            
+            
+
+            $request->getSession()->getFlashBag()->add('notice', 'Message envoyÃ© avec succÃ©s!');
 
             return $this->redirect($this->generateUrl('rs_core_homepage' ));
         }
 
-        //On passe la méthode createView() du formulaire a la vue pour l'afficher
+        //On passe la mÃ©thode createView() du formulaire a la vue pour l'afficher
         return $this->render('RSCoreBundle:Core:index.html.twig', array(
             'form'  => $form->createView(),
 
